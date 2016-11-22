592789d3c9477253dd7719a79d913f5050aaa3f8
Wolfgang Goedel <wgoedel@gmail.com>
2016-11-22 08:09:18 +0100

    08-about-modules - solved

--- src/08-about-modules.ts
+++ src/08-about-modules.ts
@@ -1,25 +1,26 @@
 import { expect } from 'chai';
 
-// _
+import Validator, {ZipCodeValidator, LettersOnlyValidator as LettersValidator} from './lib/validators';
+import * as validators from './lib/validators';
 
 describe('about modules', () => {
   it('1-can import a class exported by the module', () => {
     var validator = new ZipCodeValidator();
-    expect(validator.isAcceptable('12345')).to.be._;
+    expect(validator.isAcceptable('12345')).to.be.true;
   });
 
   it('2-can import a default export', () => {
     var validator = new Validator();
-    expect(validator.isAcceptable('12345')).to.be._;
+    expect(validator.isAcceptable('12345')).to.be.true;
   });
 
   it('3-can rename imports', () => {
     var validator = new LettersValidator();
-    expect(validator.isAcceptable('12345')).to.be._;
+    expect(validator.isAcceptable('12345')).to.be.false;
   });
 
   it('4-can import everything', () => {
-    expect(validators.lettersRegexp.test('ABCDE')).to.be._;
-    expect(validators.numberRegexp.test('12345')).to.be._;
+    expect(validators.lettersRegexp.test('ABCDE')).to.be.true;
+    expect(validators.numberRegexp.test('12345')).to.be.true;
   });
 });
